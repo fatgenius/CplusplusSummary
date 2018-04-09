db888ed69068df66b9e35e19a866b9e2ae6bbc2e
fatgenius <dbtlive@gmail.com>
2018-04-09 16:03:49 +0200

    vector and iterator

---
+++ vector.cpp
@@ -0,0 +1,13 @@
+#include "vector.h"
+
+int main ()
+{
+  vector<int> vInts; 
+  vInts.assign(10, 5);
+  printVector(vInts);
+  assignFunction(vInts, 1);
+  pushBackFunction(vInts, 2);
+  emplaceFunction(vInts,1, 3);
+
+  return 0;
+}
